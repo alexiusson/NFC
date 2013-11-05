73877ce436a7d4c5c6e33cc681897adb1bde1be2
sebastianjohansson <sebastianjohansson@172.20.10.2>
2013-11-05 12:12:24 +0100

    sfg

--- MainActivity/src/com/example/nfc/MainActivity.java
+++ MainActivity/src/com/example/nfc/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.nfc;
 
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 //import android.widget.Toast;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	NfcAdapter mNfcAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,18 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		
+		// Check for available NFC Adapter
+		mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+		if (mNfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
+//            finish();
+            return true;
+        } else{
+            Toast.makeText(this, "NFC is available", Toast.LENGTH_LONG).show();
+        }
+		
+		
 		return true;
 	}
 	
